4255a9e00e6ee57fa24ea0284ec014e0f741a778
Irene Mutua <mutuamirene.gmail.com>
2018-11-09 07:14:17 +0300

    Made modifications

--- js/main.js
+++ js/main.js
@@ -178,6 +178,8 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.tabIndex = "3";
+  more.setAttribute('aria-label', `More details about ${restaurant.name}`)
   li.append(more)
 
   return li
